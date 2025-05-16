1e7d04765d1dbc64c9105e3560ba26f2033b35aa
devvk <info@sekretov.net>
2025-05-16 17:13:09 +0200

    Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -0,0 +1,93 @@
+package ru.job4j.grabber.model;
+
+import java.util.Objects;
+
+public class Post {
+    /**
+     * Идентификатор вакансии (берется из нашей базы данных).
+     */
+    private long id;
+
+    /**
+     * Название вакансии.
+     */
+    private String title;
+
+    /**
+     * Ссылка на описание вакансии.
+     */
+    private String link;
+
+    /**
+     * Описание вакансии.
+     */
+    private String description;
+
+    /**
+     * Дата создания вакансии. Храниться в миллисекундах. В базе должно быть поле Timestamp without time zone.
+     */
+    private Long time;
+
+    public Post(long id, String title, String link, String description, Long time) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link)
+                && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link, description);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+}
